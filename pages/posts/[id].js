fd7e57966de38fc0cebf229918d13743d973fbec
Salevieno <salvio_almeida@hotmail.com>
2023-05-30 22:09:08 -0300

    Resolução do desafio - chamada de post por id

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,10 +3,7 @@ import {
   getPostBySlug,
 } from '../../utils/mdx-utils';
 
-import { MDXRemote } from 'next-mdx-remote';
 import Head from 'next/head';
-import Link from 'next/link';
-import ArrowIcon from '../../components/ArrowIcon';
 import CustomLink from '../../components/CustomLink';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
@@ -14,10 +11,7 @@ import Layout, { GradientBackground } from '../../components/Layout';
 import SEO from '../../components/SEO';
 
 
-const components = {
-  a: CustomLink,
-  Head,
-};
+const components = {a: CustomLink, Head};
 
 export default function PostPage({
   posts,
@@ -25,19 +19,14 @@ export default function PostPage({
 }) {
   return (
     <Layout>
-      <SEO
-        title={`${posts.title} - ${globalData.name}`}
-        description={posts.description}
-      />
+      <SEO title={`${posts.title} - ${globalData.name}`} description={posts.description} />
       <Header name={globalData.name} />
       <article className="px-6 md:px-0">
         <header>
           <h1 className="text-3xl md:text-5xl dark:text-white text-center mb-12">
             {posts?.title}
           </h1>
-          {posts?.description && (
-            <p className="text-xl mb-4">{posts?.description}</p>
-          )}
+          {posts?.description && (<p className="text-xl mb-4">{posts?.description}</p>)}
         </header>
         <main>
           <article className="prose dark:prose-dark">
@@ -46,28 +35,16 @@ export default function PostPage({
         </main>
       </article>
       <Footer copyrightText={globalData.footerText} />
-      <GradientBackground
-        variant="large"
-        className="absolute -top-32 opacity-30 dark:opacity-50"
-      />
-      <GradientBackground
-        variant="small"
-        className="absolute bottom-0 opacity-20 dark:opacity-10"
-      />
+      <GradientBackground variant="large" className="absolute -top-32 opacity-30 dark:opacity-50" />
+      <GradientBackground variant="small" className="absolute bottom-0 opacity-20 dark:opacity-10" />
     </Layout>
   );
 }
 
 export const getServerSideProps = async ({ params }) => {
   const globalData = getGlobalData();
-  const posts = await getPostBySlug(params.id);
- 
+  const posts = await getPostBySlug(params.id); 
 
-  return {
-    props: {
-      globalData,
-      posts,
-    },
-  };
+  return {props: {globalData, posts}};
 };
 
--- services/api.js
+++ services/api.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
-export const api  = axios.create({
-    baseURL: 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1',
+export const api = axios.create({
+    baseURL: 'https://wdfljsdujpzloxeuxtda.supabase.co/rest/v1',
     headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA"
-    }
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndkZmxqc2R1anB6bG94ZXV4dGRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODU0ODU0OTAsImV4cCI6MjAwMTA2MTQ5MH0.KwAEB9rYOX2v6gbyMGYJz0pTkjhAudLdJQTJja73tso",
+        Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndkZmxqc2R1anB6bG94ZXV4dGRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODU0ODU0OTAsImV4cCI6MjAwMTA2MTQ5MH0.KwAEB9rYOX2v6gbyMGYJz0pTkjhAudLdJQTJja73tso"}
 })
